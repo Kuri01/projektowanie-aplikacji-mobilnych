b6678f4255751e7d73b7f9845dc73bc1657febfe
Kamil Kuriata <kamil.kuriata01@gmail.com>
2024-11-26 07:54:28 +0100

    feat: add zad-3-intencje functionality

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
-import React from "react";
-import Calculator from "./components/Calculator";
-import {PaperProvider} from "react-native-paper";
+import './src/i18n';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { useTranslation } from 'react-i18next';
+import FormScreen from './src/FormScreen';
+import SummaryScreen from './src/SummaryScreen';
+import { RootStackParamList } from './src/types';
 
-export default function App() {
+const Stack = createStackNavigator<RootStackParamList>();
+
+const App: React.FC = () => {
+    const { t } = useTranslation();
+
     return (
         <PaperProvider>
-            <Calculator />
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Form">
+                    <Stack.Screen
+                        name="Form"
+                        component={FormScreen}
+                        options={{ title: t('formTitle') }}
+                    />
+                    <Stack.Screen
+                        name="Summary"
+                        component={SummaryScreen}
+                        options={{ title: t('summaryTitle') }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
         </PaperProvider>
     );
-}
+};
+
+export default App;
